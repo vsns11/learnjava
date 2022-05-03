f23c8ff94ca1e62f48d4b3d324bdae6743eccd98
sivanagasaivemula <vemula.sivanagasai@gmail.com>
2022-05-02 21:06:32 -0400

    Add custom annotation

--- src/main/java/ca/siva/ds/misc/MyAnnotation.java
+++ src/main/java/ca/siva/ds/misc/MyAnnotation.java
@@ -1,8 +1,46 @@
 package ca.siva.ds.misc;
 
+import java.lang.annotation.*;
+
 /**
  * YOU CANNOT USE COMPLEX DATATYPES INSIDE ANNOTATION
+ * 3 types of compile time annotations
+ * 1) Deprecated
+ * 2) SuppressWarnings
+ * 3) Contended
+ * 4) Override
+ *
  */
+
+/**
+ * possible RetentionPolicies:
+ * 1. RetentionPolicy.CLASS
+ *      default policy applied if none mentioned, it means annotation is stored in .class file
+ *      but not available during runtime.
+ * 2. RetentionPolicy.SOURCE
+ *       annotation is only available in source file, not in .class file. not during runtime too.
+ *       It is used mainly with build tools
+ * 3. RetentionPolicy.RUNTIME
+ *      Annotation is available during runtime.
+ */
+@Retention(RetentionPolicy.CLASS)
+/**
+ * ElementType.ANNOTATION_TYPE
+ * ElementType.CONSTRUCTOR
+ * ElementType.FIELD
+ * ElementType.LOCAL_VARIABLE
+ * ElementType.METHOD
+ * ElementType.PACKAGE
+ * ElementType.PARAMETER
+ * ElementType.TYPE
+ * ElementType.TYPE_PARAMETER
+ * ElementType.TYPE_USE
+ */
+@Target(ElementType.TYPE)
+/**
+ * the subclass also inherits the annotation, when the super class applies this annotation.
+ */
+@Inherited
 @interface MyAnnotation {
 
     String value() default "";
---
+++ src/main/java/ca/siva/ds/misc/MyClass.java
@@ -0,0 +1,11 @@
+package ca.siva.ds.misc;
+
+@MyAnnotation(
+        name = "Siva",
+        age = 25,
+        newNames = {"Siva", "Vemula"}
+)
+public class MyClass {
+
+
+}
