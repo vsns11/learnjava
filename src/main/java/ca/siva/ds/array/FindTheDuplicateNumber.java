9219edfb6629f086043452b12662460b99c7350b
sivanagasaivemula <vemula.sivanagasai@gmail.com>
2022-05-13 10:37:17 -0400

    Add 287

---
+++ src/main/java/ca/siva/ds/array/FindTheDuplicateNumber.java
@@ -0,0 +1,26 @@
+package ca.siva.ds.array;
+
+/**
+ * Leetcode 287
+ */
+public class FindTheDuplicateNumber {
+    class Solution {
+        public int findDuplicate(int[] nums) {
+            int t = nums[0], h = nums[0];
+
+            do {
+                t = nums[t];
+                h = nums[nums[h]];
+            } while (t != h);
+
+
+            t = nums[0];
+
+            while (t != h) {
+                t = nums[t];
+                h = nums[h];
+            }
+            return t;
+        }
+    }
+}
