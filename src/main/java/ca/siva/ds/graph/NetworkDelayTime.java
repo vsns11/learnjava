55bf1fe1558005cf46eda727013b599eaa41bbe6
sivanagasaivemula <vemula.sivanagasai@gmail.com>
2022-05-14 16:56:53 -0400

    Add dijkstra approach

--- src/main/java/ca/siva/ds/graph/NetworkDelayTime.java
+++ src/main/java/ca/siva/ds/graph/NetworkDelayTime.java
@@ -11,7 +11,7 @@ public class NetworkDelayTime {
      * Approach DFS, time: O( (N-1)! + ElogE)
      *  Space: O(N + E)
      */
-    class Solution {
+    class Solution1 {
 
         class Pair<U, V> {
             U t; // destination
@@ -68,4 +68,84 @@ public class NetworkDelayTime {
 
         }
     }
+
+    /**
+     * Approach Dijkstra, time: O(N + E log N)
+     *  Space: O(N + E)
+     */
+    class Solution2 {
+
+        class Pair<U, V> {
+            U d; // destination
+            V t; // time
+
+            public Pair(U d, V t) {
+                this.d = d;
+                this.t = t;
+            }
+
+            public V getT() {
+                return this.t;
+            }
+
+
+        }
+
+        Map<Integer, List<Pair<Integer, Integer>> > adj = new HashMap<>();
+
+        public void doDijkStra(int[] recvTimes, int k) {
+            Queue<Pair<Integer, Integer>> pq = new
+                    PriorityQueue<Pair<Integer, Integer>>(
+                    Comparator.comparing(Pair::getT)
+            );
+
+            recvTimes[k] = 0;
+            pq.add(new Pair(k, 0));
+
+            while (!pq.isEmpty()) {
+                Pair<Integer, Integer> item = pq.remove();
+
+                // iterate all the adjacent pairs.
+                for (Pair<Integer, Integer> adjItem: adj.get(item.d)) {
+                    Integer calcReachTime = adjItem.t + item.t;
+                    if (calcReachTime < recvTimes[adjItem.d]) {
+                        recvTimes[adjItem.d] = calcReachTime;
+                        pq.offer(new Pair(adjItem.d, calcReachTime));
+                    }
+                }
+
+            }
+
+        }
+
+
+
+        public int networkDelayTime(int[][] times, int n, int k) {
+
+            for (int[] t: times) {
+                int source = t[0], destination = t[1], time=t[2];
+                adj.putIfAbsent(source, new ArrayList<>());
+                adj.putIfAbsent(destination, new ArrayList<>());
+                adj.get(source).add(new Pair(destination, time));
+            }
+
+            int[] recvTimes = new int[n + 1];
+            Arrays.fill(recvTimes, Integer.MAX_VALUE);
+
+
+            doDijkStra(recvTimes, k);
+            int result = -1;
+
+            // System.out.println(Arrays.toString(recvTimes));
+
+            for (int i=1; i < recvTimes.length; ++i) {
+                result = Math.max(result, recvTimes[i]);
+            }
+
+
+            return result == Integer.MAX_VALUE ? -1: result;
+        }
+
+    }
+
 }
