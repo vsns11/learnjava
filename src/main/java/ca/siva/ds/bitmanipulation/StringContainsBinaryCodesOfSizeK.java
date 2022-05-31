23e5a2065e4de3d55ed08dd95019e2d8933a170d
sivanagasaivemula <vemula.sivanagasai@gmail.com>
2022-05-30 21:43:05 -0400

    Add optimal approach

--- src/main/java/ca/siva/ds/bitmanipulation/StringContainsBinaryCodesOfSizeK.java
+++ src/main/java/ca/siva/ds/bitmanipulation/StringContainsBinaryCodesOfSizeK.java
@@ -24,6 +24,33 @@ public class StringContainsBinaryCodesOfSizeK {
             return false;
         }
 
+        /**
+         * Time: O(N), Space: O(2^k)
+         */
+        class Solution2 {
+            // Rolling hash approach
+            public boolean hasAllCodes(String s, int k) {
+                int totalCount = 1 << k;
+                int allOnes = totalCount - 1;
+                int hash = 0;
+                boolean[] cache = new boolean[totalCount];
+
+                for (int i = 0; i < s.length(); ++i) {
+                    char ch = s.charAt(i);
+                    hash = ((hash << 1) & allOnes) | ch - '0';
+                    if ( i + 1 >= k && !cache[hash]) {
+                        cache[hash] = true;
+                        totalCount--;
+                        if (totalCount == 0) return true;
+                    }
+                }
+
+                return false;
+
+            }
+
+
+        }
 
     }
 }
