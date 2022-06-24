e28af833714193e7d55b980e1785fc43aa39e259
sivanagasaivemula <vemula.sivanagasai@gmail.com>
2022-06-24 02:41:10 -0400

    use annotation

---
+++ src/main/java/ca/siva/ds/annotation/Data.java
@@ -0,0 +1,14 @@
+package ca.siva.ds.annotation;
+
+@StudentInfo(name = "Siva", age = 25)
+public class Data {
+
+    public static void main(String[] args) {
+        Data d = new Data();
+        Class<? extends Object> clazz = d.getClass();
+        StudentInfo x = clazz.getAnnotation(StudentInfo.class);
+        System.out.println(x.age());
+        System.out.println(x.name());
+    }
+
+}
