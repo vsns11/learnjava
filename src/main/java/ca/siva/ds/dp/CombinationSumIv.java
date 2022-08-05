bf195f4e97d9a223e41baa26554766e1539b4800
sivanagasaivemula <vemula.sivanagasai@gmail.com>
2022-08-05 01:16:32 -0400

    Add approach2

--- src/main/java/ca/siva/ds/dp/CombinationSumIv.java
+++ src/main/java/ca/siva/ds/dp/CombinationSumIv.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CombinationSumIv {
+    //Leetcode: 377, Time: O(T*N), Space: O(T)
     class Solution {
         public int combinationSum4(int[] nums, int target) {
             Map<Integer, Integer> ref = new HashMap<>();
@@ -24,5 +25,24 @@ public class CombinationSumIv {
             return count;
         }
 
+    }
+
+    //Approach 2 with DP, same time and space complexity
+
+    class Solution2 {
+        public int combinationSum4(int[] nums, int target) {
+            int[] dp = new int[target + 1];
+            dp[0] = 1;
+
+            for (int i = 1; i < target + 1; ++i ) {
+                for (int num: nums) {
+                    if (i - num >= 0) {
+                        dp[i] += dp[i - num];
+                    }
+                }
+
+            }
+            return dp[target];
+        }
     }
 }
