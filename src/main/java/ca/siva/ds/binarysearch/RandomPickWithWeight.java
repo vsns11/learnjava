a628b40a73975a8fee597e5eae57385eb6276781
sivanagasaivemula <vemula.sivanagasai@gmail.com>
2022-08-11 02:28:22 -0400

    Add 528

---
+++ src/main/java/ca/siva/ds/binarysearch/RandomPickWithWeight.java
@@ -0,0 +1,36 @@
+package ca.siva.ds.binarysearch;
+
+public class RandomPickWithWeight {
+    //Leetcode: O(N + log n), Space: O(N)
+    class Solution {
+        private int[] prefixSums;
+        private int totalSum;
+
+        public Solution(int[] w) {
+            this.prefixSums = new int[w.length];
+
+            int prefixSum = 0;
+            for (int i = 0; i < w.length; ++i) {
+                prefixSum += w[i];
+                this.prefixSums[i] = prefixSum;
+            }
+            this.totalSum = prefixSum;
+        }
+
+        public int pickIndex() {
+            double target = this.totalSum * Math.random();
+
+            // run a binary search to find the target zone
+            int low = 0, high = this.prefixSums.length;
+            while (low < high) {
+                // better to avoid the overflow
+                int mid = low + (high - low) / 2;
+                if (target > this.prefixSums[mid])
+                    low = mid + 1;
+                else
+                    high = mid;
+            }
+            return low;
+        }
+    }
+}
